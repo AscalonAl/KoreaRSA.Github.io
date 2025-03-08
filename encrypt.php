5b1d2db920de22b90279ab15169d1d0fa55fc5c4
AscalonAl <marcelino.mangare@gmail.com>
2025-03-08 02:51:15 -0800

    Add files via upload

---
+++ encrypt.php
@@ -0,0 +1,34 @@
+<?php
+
+function encipher($publicKey, $plainText) {
+    $cipherText = [];
+    $n = 3337; // The modulus value from the original Python code
+    foreach (str_split($plainText) as $char) {
+        $i = ord($char);
+        $cipher = ($i ** $publicKey) % $n;
+        $cipherText[] = $cipher;
+    }
+    return implode(' ', $cipherText);
+}
+
+function decipher($privateKey, $cipherText) {
+    $plainText = '';
+    $n = 3337; // The modulus value from the original Python code
+    $cipherArray = explode(' ', $cipherText);
+    foreach ($cipherArray as $cipher) {
+        $i = (int)$cipher;
+        $plainChar = chr(($i ** $privateKey) % $n);
+        $plainText .= $plainChar;
+    }
+    return $plainText;
+}
+
+$publicKey = 79;
+$privateKey = 1019;
+
+$inputText = $_POST['text'];
+$cipherText = encipher($publicKey, $inputText);
+echo "<h2>Encrypted Text:</h2><p>$cipherText</p>";
+
+
+?>
